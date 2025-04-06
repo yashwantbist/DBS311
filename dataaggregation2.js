757b2606669823f1c00b15b268f34d81194de155
Chadcel <denisb1135@gmail.com>
2025-04-06 15:36:40 -0400

    Adapt Yashwant's code for data aggregation 2

--- dataaggregation2.js
+++ dataaggregation2.js
@@ -2,3 +2,36 @@ let db = connect('mongodb://localhost/dbs311_a2');
 
 // code here
 // remember to wrap in a try catch block!
+
+// adapted from Yashwant's code
+
+try {
+	console.log(db.rentals.aggregate([
+		{
+			$project : {
+				month : {$month : "$date_rented"},
+				year : {$year : "$date_rented"},
+				price_charged : 1
+			}
+		},
+		{
+			$group : {
+				_id : {
+					year : "$year",
+					month : "$month"
+				},
+				total_charge : {
+					$sum: "$price_charged"
+				}
+			}
+		},
+		{
+			$sort : {
+				"_id.year" : 1,
+				"_id.month" : 1
+			}
+		}
+	]));
+} catch (error) {
+	console.log(error);
+}
